7249b1369c5fe39427a9fbce2c8dce8c5919a468
Fadilix <iamfadile2@gmail.com>
2026-02-01 23:37:21 +0100

    refactor: moved history displaying to helper function (#8)

--- cmd/couik/main.go
+++ cmd/couik/main.go
@@ -6,7 +6,6 @@ import (
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/fadilix/couik/cmd/couik/cli"
-	"github.com/fadilix/couik/database"
 	"github.com/fadilix/couik/pkg/typing"
 	"github.com/fadilix/couik/pkg/ui"
 )
@@ -19,22 +18,7 @@ func main() {
 	}
 
 	if cli.History {
-		fmt.Println("Couik History")
-		history, err := database.GetHistory()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		for i := range history {
-			fmt.Printf("Test #%d\n", i+1)
-			fmt.Printf("Speed: %.2f\n", history[i].WPM)
-			fmt.Printf("Accuracy: %.2f\n", history[i].Acc)
-			fmt.Printf("Raw speed: %.2f\n", history[i].RawWPM)
-			fmt.Printf("Duration: %.2f seconds\n", history[i].Duration.Seconds())
-			fmt.Printf("Quote: %s (%d characters)\n", history[i].Quote, len(history[i].Quote))
-			fmt.Printf("Date: %s\n", history[i].Date.Format("02 Jan 2006"))
-			fmt.Println()
-		}
+		cli.DisplayHistory()
 		return
 	}
 
